e5e02629391fe6fd2c48bea9893ae3adf560bf69
mkl <mkl48@nau.edu>
2013-10-21 09:07:51 -0400

    updates

--- projects/lichen_coo/src/onc_stand_ses.R
+++ projects/lichen_coo/src/onc_stand_ses.R
@@ -7,10 +7,6 @@ garden[garden=='P'] <- 'pit'
 garden[garden!='pit'] <- 'onc'
                                         #separate onc
 onc <- garden.data[garden=='onc',]
-					#tree overlap between years
-unique(onc$Tree[onc$Year=='2010']) %in% unique(onc$Tree[onc$Year=='2011'])
-unique(onc$Tree[onc$Year=='2011']) %in% unique(onc$Tree[onc$Year=='2010'])
-onc <- onc[onc$Year=='2011',]
 					#
 stand.null <- nullCom(onc[,7:15])
 stand.null <- lapply(stand.null,cscore)
