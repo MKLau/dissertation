e1207a75598eb6ceffce2070a27ea84890631fe1
mkl <mkl48@nau.edu>
2013-10-29 17:07:02 -0400

    Fixed data input bug in onc.R

--- projects/lichen_coo/src/onc.R
+++ projects/lichen_coo/src/onc.R
@@ -10,12 +10,14 @@ source('./helper_funcs.R')
 
 ###Garden Analysis
 garden.data <- read.csv('~/projects/dissertation/projects/lichen_coo/data/LCO_data_ONC_PIT.csv')
-garden <- substr(garden.data[,1],2,2)
-garden[garden=='P'] <- 'pit'
-garden[garden!='pit'] <- 'onc'
-
+                                        #remove genotype RL6 and N1.31
+garden.data <- garden.data[garden.data$Geno!='RL6',]
+garden.data <- garden.data[garden.data$Tree!='N1.31',]
                                         #separate onc
-onc <- garden.data[garden=='onc',]
+garden.data[,1] <- as.character(garden.data[,1])
+g1 <- substr(garden.data[,1],2,2)
+g1[g1!='P'] <- 'onc'
+onc <- garden.data[g1=='onc',]
 					#tree overlap between years
 unique(onc$Tree[onc$Year=='2010']) %in% unique(onc$Tree[onc$Year=='2011'])
 unique(onc$Tree[onc$Year=='2011']) %in% unique(onc$Tree[onc$Year=='2010'])
@@ -24,6 +26,7 @@ unique(onc$Tree[onc$Year=='2011']) %in% unique(onc$Tree[onc$Year=='2010'])
 Phy <- apply(onc[,12:14],1,sum) #make phy out of pmel,pads,pund
 onc. <- onc[,-12:-14]
 onc <- data.frame(onc.,Phy)
+if (all(table(onc[,1])==100)){}else{for (i in 1:1000){print('Warning: check input data!!!')}}
 ###Composition with height
 library(vegan)
 com <- split(onc[,7:ncol(onc)],paste(onc[,1],onc[,3],onc[,4]))
@@ -57,12 +60,11 @@ onc.q <- lapply(onc.q,function(x) x[,7:ncol(x)])
 obs.cs <- unlist(lapply(onc.q,cscore))
 onc.ses <- dget('../data/onc_tree_ses.Rdata')
 onc.p <- dget('../data/onc_tree_pval.Rdata')
-ses.zero.p <- FALSE
-ses.zero.sd2 <- TRUE
+ses.zero.p <- TRUE;ses.zero.sd2 <- FALSE
 if (ses.zero.p){onc.ses[onc.p>0.05] <- 0}else{}
 if (ses.zero.sd2){onc.ses[abs(onc.ses) < 2] <- 0}else{}
 onc.tn <- lapply(onc.q,dep.net) #tree level networks
-names(onc.ses) <- names(onc.q)
+names(onc.ses) <- names(onc.q)[names(onc.q)!='']
 onc.ses[is.na(onc.ses)] <- 0
 onc.centrality <- unlist(lapply(onc.tn,function(x) centralization(x,FUN='degree')))
 onc.deg <- unlist(lapply(onc.tn,function(x) length(x[x!=0])))
@@ -87,77 +89,3 @@ genotype <- as.character(sapply(names(onc.q),function(x) unlist(strsplit(x,split
                                         #remove outlier trees
 rm.n1.31 <- (names(onc.ses)!='N1.31 2011 1008')
 rm.rl6 <- (genotype!='RL6')
-                                        #roughness by genotype
-summary(aov(avg.rough~genotype))
-summary(aov(avg.rough[rm.n1.31&rm.rl6]~genotype[rm.n1.31&rm.rl6]))
-                                        #centrality
-plot(sqrt(onc.centrality)~factor(genotype))
-summary(aov(onc.ses[names(onc.ses)!='N1.31 2011 1008']~genotype[names(onc.ses)!='N1.31 2011 1008']))
-plot(onc.ses[names(onc.ses)!='N1.31 2011 1008']~factor(genotype[names(onc.ses)!='N1.31 2011 1008']))
-                                        #
-geno.rm <- c('H10','RL6','T6','WC5')
-rm.n1.31 <- (names(onc.ses)!='N1.31 2011 1008')
-rm.rl6 <- (genotype!='RL6')
-outlier.rm <- ((names(onc.ses)!='N1.31 2011 1008')&(genotype%in%geno.rm==FALSE))
-summary(aov(onc.ses[outlier.rm]~genotype[outlier.rm]))
-plot(onc.ses[outlier.rm]~factor(genotype[outlier.rm]))
-                                        #
-summary(aov(sqrt(onc.centrality[outlier.rm])~genotype[outlier.rm]))
-plot(onc.centrality[outlier.rm]~factor(genotype[outlier.rm]))
-                                        #
-summary(aov(log(onc.deg+0.001)~genotype))
-plot(onc.deg~factor(genotype))
-summary(aov(log(onc.deg[outlier.rm]+0.001)~genotype[outlier.rm]))
-plot(onc.deg[outlier.rm]~factor(genotype[outlier.rm]))
-###Removed N1.31 and RL6
-                                        #roughness ~ genotype 
-summary(aov(avg.rough[rm.n1.31&rm.rl6]~genotype[rm.n1.31&rm.rl6]))
-plot(avg.rough[rm.n1.31&rm.rl6]~factor(genotype[rm.n1.31&rm.rl6]))
-                                        #ses and centrality correlation
-summary(lm(onc.ses[rm.n1.31&rm.rl6]~onc.deg[rm.n1.31&rm.rl6]))
-plot(onc.ses[rm.n1.31&rm.rl6]~onc.deg[rm.n1.31&rm.rl6])
-                                        #
-summary(aov((log(abs(onc.ses[rm.n1.31&rm.rl6])+0.1))~genotype[rm.n1.31&rm.rl6]))
-plot(onc.ses[rm.n1.31&rm.rl6]~factor(genotype[rm.n1.31&rm.rl6]))
-summary(aov((log(abs(onc.ses[rm.n1.31&rm.rl6])+0.1))~avg.rough[rm.n1.31&rm.rl6]))
-                                        #
-summary(aov(sqrt(onc.centrality[rm.n1.31&rm.rl6])~genotype[rm.n1.31&rm.rl6]))
-plot(onc.centrality[rm.n1.31&rm.rl6]~factor(genotype[rm.n1.31&rm.rl6]))
-summary(aov(sqrt(onc.centrality[rm.n1.31&rm.rl6])~avg.rough[rm.n1.31&rm.rl6]))
-plot(onc.centrality[rm.n1.31&rm.rl6]~factor(avg.rough[rm.n1.31&rm.rl6]))
-summary(aov((onc.centrality[rm.n1.31&rm.rl6])~avg.rough[rm.n1.31&rm.rl6]))
-                                        #
-summary(aov(log(onc.deg[rm.n1.31&rm.rl6]+0.001)~genotype[rm.n1.31&rm.rl6]))
-plot(onc.deg[rm.n1.31&rm.rl6]~factor(genotype[rm.n1.31&rm.rl6]))
-summary(lm((onc.deg[rm.n1.31&rm.rl6])^2~avg.rough[rm.n1.31&rm.rl6]))
-plot(onc.deg[rm.n1.31&rm.rl6]~avg.rough[rm.n1.31&rm.rl6])
-                                        #deg~rough
-summary(aov(sqrt(onc.deg[rm.n1.31&rm.rl6])~avg.rough[rm.n1.31&rm.rl6]))                                        
-                                        #network similarity
-net.dist <- matrix(0,nrow=length(onc.tn[rm.n1.31&rm.rl6]),ncol=length(onc.tn[rm.n1.31&rm.rl6]))
-for (i in 1:nrow(net.dist)){
-  for (j in 1:ncol(net.dist)){
-    x <- onc.tn[rm.n1.31&rm.rl6][[i]]
-    y <- onc.tn[rm.n1.31&rm.rl6][[j]]
-    net.dist[i,j] <- sqrt(sum((x - y)^2))
-  }
-}
-net.d <- as.dist((net.dist))
-adonis(net.d~factor(genotype[rm.n1.31&rm.rl6]),perm=5000)
-adonis(net.d~avg.rough[rm.n1.31&rm.rl6],perm=5000)
-                                        #composition
-adonis(com.rel[rm.n1.31&rm.rl6,]~avg.rough[rm.n1.31&rm.rl6],perm=5000)
-                                        #look at compositional effect of genotype
-com <- do.call(rbind,lapply(onc.q,function(x) apply(x,2,sum)))
-com. <- cbind(com,ds=rep(1,nrow(com)))
-com.rel <- apply(com.,2,function(x) x/max(x))
-com.rel[,ncol(com.rel)] <- com.rel[,ncol(com.rel)] * 0.001
-adonis(com.~factor(genotype))
-adonis(com.~avg.rough)
-adonis(com.rel~factor(genotype))
-adonis(com.rel~avg.rough)
-
-###SEM
-###Can we get genotype averages?
-
-###NEED TO RECONCILE TAXONOMIC DIFFERENCES
---
+++ projects/lichen_coo/src/onc_ses.R
@@ -0,0 +1,42 @@
+##Generate the stand level ses values
+source('./seenetR.R')
+library(vegan)
+library(pbapply)
+print('Loading data')
+garden.data <- read.csv('../data/LCO_data_ONC_PIT.csv')
+garden <- substr(garden.data[,1],2,2)
+garden[garden=='P'] <- 'pit'
+garden[garden!='pit'] <- 'onc'
+                                        #separate onc
+print('ONC analyses')
+onc <- garden.data[garden=='onc',]
+###Merge species groups
+print('Merge physcia species')
+Phy <- apply(onc[,12:14],1,sum) #make phy out of pmel,pads,pund
+onc. <- onc[,-12:-14]
+onc <- data.frame(onc.,Phy)
+					#tree level
+print('Tree level co-occurrence')
+onc.q <- split(onc,paste(onc[,1],onc[,3],onc[,2]))
+onc.q <- lapply(onc.q,function(x) x[,7:ncol(x)])
+obs.cs <- unlist(lapply(onc.q,cscore))
+onc.sim <- pblapply(onc.q,function(x) if (sum(sign(apply(x,2,sum)))>1){nullCom(x)}else{NA})
+onc.cs <- pblapply(onc.sim,function(x) if (any(is.na(x[[1]]))){NA}else{lapply(x,cscore)})
+onc.cs <- pblapply(onc.cs,unlist)
+onc.ses <- obs.cs*0
+onc.p <- obs.cs*0
+for (i in 1:length(onc.ses)){
+  print(i)
+  onc.ses[i] <- (obs.cs[i] - mean(onc.cs[[i]])) / sd(onc.cs[[i]])
+  onc.p[i] <- length(onc.cs[[i]][onc.cs[[i]]<=obs.cs[i]])/length(onc.cs[i])
+}
+print('Writing to: ../data/onc_tree_ses.Rdata')
+dput(onc.cs,'../data/onc_tree_cs.Rdata')
+dput(onc.ses,'../data/onc_tree_ses.Rdata')
+dput(onc.p,'../data/onc_tree_pval.Rdata')
+					#stand level
+# print('Stand level co-occurrence')
+# stand.null <- nullCom(onc[,7:ncol(onc)])
+# stand.null <- lapply(stand.null,cscore)
+# print('Writing to: ../data/onc_stand_null.Rdata')
+# dput(stand.null,file='../data/onc_stand_null.Rdata')
