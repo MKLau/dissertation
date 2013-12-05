2a9a09693b44f7dcd07ebea0671211322e280b7b
mkl <mkl48@nau.edu>
2013-12-05 08:18:52 -0500

    update

--- projects/art_coo/src/acn_analyses.R
+++ projects/art_coo/src/acn_analyses.R
@@ -33,8 +33,6 @@ pb <- pit$pb.upper + pit$pb.lower
 pit <- cbind(pit,pb=pb)
 pit$pb.pred <- pit$pb.pred + pit$pb.hole + pit$pb.woody.pred
 pit <- pit[,colnames(pit)!='pb.woody'&colnames(pit)!='pb.woody.pred'&colnames(pit)!='pb.hole'&colnames(pit)!='mite'&colnames(pit)!='pb.upper'&colnames(pit)!='pb.lower']
-                                        #remove fungal
-pit <- pit[,colnames(pit)!='fungal']
                                         #remove species with less than 17 observations
 pit.com <- pit[,-1:-6]
 pit.com <- pit.com[,apply(pit.com,2,sum)>17]
@@ -45,21 +43,6 @@ sen <- pit.com[pit[,1]=='sen',]
 liv.tl <- split(liv,pit$tree[pit[,1]=='live'])
 sen.tl <- split(sen,pit$tree[pit[,1]=='sen'])
 all(names(liv.tl)==names(sen.tl))
-###Pemphigus patterns
-                                        #pemphigus abundances
-pb.liv <- unlist(lapply(liv.tl,function(x) sum(x$pb)/length(x$pb)))
-pb.sen <- unlist(lapply(sen.tl,function(x) sum(x$pb)/length(x$pb)))
-plot(pb.sen~pb.liv)
-abline(lm(pb.sen~pb.liv))
-hist((pb.sen-pb.liv))
-t.test((pb.sen-pb.liv))
-                                        #fungal
-fung.liv <- unlist(lapply(liv.tl,function(x) sum(x$fungal)/length(x$fungal)))
-fung.sen <- unlist(lapply(sen.tl,function(x) sum(x$fungal)/length(x$fungal)))
-plot(fung.sen~fung.liv)
-abline(lm(fung.sen~fung.liv))
-hist((fung.sen-fung.liv))
-t.test((fung.sen-fung.liv))
 
 ###Live leaves
 ###Stand level
@@ -70,27 +53,6 @@ sen.dnet <- dep.net(sen)
 mgp2(sen.dnet,(apply(sen,2,sum)/max(apply(sen,2,sum)))+1,log.scale=FALSE,my.coord=coord)
 
 ###Tree level
-                                        #with fungus
-ses.liv <- dget('../data/acn_ses_live.Rdata')
-ses.sen <- dget('../data/acn_ses_sen.Rdata')
-ses.liv[is.na(ses.liv)] <- 0
-ses.sen[is.na(ses.sen)] <- 0
-all(names(ses.liv)==names(ses.sen))
-geno.ses <- unlist(sapply(names(ses.liv),function(x) strsplit(x,split=' ')[[1]][2]))
-t.test(I(ses.sen-ses.liv))
-hist(I(ses.sen-ses.liv))
-summary(aov(ses.liv~geno.ses))
-summary(aov(ses.sen~geno.ses))
-library(gplots)
-par(mfrow=c(1,2))
-mu <- tapply(ses.liv,geno.ses,mean)
-se <- tapply(ses.liv,geno.ses,function(x) sd(x)/sqrt(length(x)))
-barplot2(mu,plot.ci=TRUE,ci.u=mu+se,ci.l=mu-se,ylab='SES',ylim=c(-5,2))
-title(main='Living')
-mu <- tapply(ses.sen,geno.ses,mean)
-se <- tapply(ses.sen,geno.ses,function(x) sd(x)/sqrt(length(x)))
-barplot2(mu,plot.ci=TRUE,ci.u=mu+se,ci.l=mu-se,ylab='SES',ylim=c(-5,2))
-title(main='Senescing')
                                         #without fungus
 ses.liv <- dget('../data/acn_ses_live_nf.Rdata')
 ses.sen <- dget('../data/acn_ses_sen_nf.Rdata')
@@ -98,17 +60,91 @@ ses.liv[is.na(ses.liv)] <- 0
 ses.sen[is.na(ses.sen)] <- 0
 all(names(ses.liv)==names(ses.sen))
 geno.ses <- unlist(sapply(names(ses.liv),function(x) strsplit(x,split=' ')[[1]][2]))
-t.test(I(ses.sen-ses.liv))
-hist(I(ses.sen-ses.liv))
 summary(aov(ses.liv~geno.ses))
 summary(aov(ses.sen~geno.ses))
 library(gplots)
 par(mfrow=c(1,2))
 mu <- tapply(ses.liv,geno.ses,mean)
 se <- tapply(ses.liv,geno.ses,function(x) sd(x)/sqrt(length(x)))
-barplot2(mu,plot.ci=TRUE,ci.u=mu+se,ci.l=mu-se,ylab='SES',ylim=c(-1,2))
+barplot2(mu,plot.ci=TRUE,ci.u=mu+se,ci.l=mu-se,ylab='SES',ylim=c(-3,3))
 title(main='Living')
 mu <- tapply(ses.sen,geno.ses,mean)
 se <- tapply(ses.sen,geno.ses,function(x) sd(x)/sqrt(length(x)))
-barplot2(mu,plot.ci=TRUE,ci.u=mu+se,ci.l=mu-se,ylab='SES',ylim=c(-1,2))
+barplot2(mu,plot.ci=TRUE,ci.u=mu+se,ci.l=mu-se,ylab='SES',ylim=c(-3,3))
 title(main='Senescing')
+                                        #geno effect of senescence
+sen.eff <- ses.sen-ses.liv
+summary(aov(sen.eff~geno.ses))
+mu <- tapply(sen.eff,geno.ses,mean)
+se <- tapply(sen.eff,geno.ses,function(x) sd(x)/sqrt(length(x)))
+par(mfrow=c(1,1))
+barplot2(mu,plot.ci=TRUE,ci.u=mu+se,ci.l=mu-se,ylab='(Sen - Liv) SES',ylim=c(-4,5))
+title(main='Senescence Effect')
+plot(TukeyHSD(aov(sen.eff~geno.ses)))
+                                        #with fungus
+ses.liv <- dget('../data/acn_ses_live.Rdata')
+ses.sen <- dget('../data/acn_ses_sen.Rdata')
+ses.liv[is.na(ses.liv)] <- 0
+ses.sen[is.na(ses.sen)] <- 0
+all(names(ses.liv)==names(ses.sen))
+geno.ses <- unlist(sapply(names(ses.liv),function(x) strsplit(x,split=' ')[[1]][2]))
+summary(aov(ses.liv~geno.ses))
+summary(aov(ses.sen~geno.ses))
+library(gplots)
+par(mfrow=c(1,2))
+mu <- tapply(ses.liv,geno.ses,mean)
+se <- tapply(ses.liv,geno.ses,function(x) sd(x)/sqrt(length(x)))
+barplot2(mu,plot.ci=TRUE,ci.u=mu+se,ci.l=mu-se,ylab='SES',ylim=c(-5,2))
+title(main='Living')
+mu <- tapply(ses.sen,geno.ses,mean)
+se <- tapply(ses.sen,geno.ses,function(x) sd(x)/sqrt(length(x)))
+barplot2(mu,plot.ci=TRUE,ci.u=mu+se,ci.l=mu-se,ylab='SES',ylim=c(-5,2))
+title(main='Senescing')
+                                        #geno effect on senescence effect
+sen.eff <- ses.sen-ses.liv
+summary(aov(sen.eff~geno.ses))
+mu <- tapply(sen.eff,geno.ses,mean)
+se <- tapply(sen.eff,geno.ses,function(x) sd(x)/sqrt(length(x)))
+par(mfrow=c(1,1))
+barplot2(mu,plot.ci=TRUE,ci.u=mu+se,ci.l=mu-se,ylab='(Sen - Liv) SES',ylim=c(-4,5))
+title(main='Senescence Effect')
+plot(TukeyHSD(aov(sen.eff~geno.ses)))
+
+###Pemphigus patterns
+                                        #pemphigus abundances
+pb.liv <- unlist(lapply(liv.tl,function(x) sum(x$pb)/length(x$pb)))
+pb.sen <- unlist(lapply(sen.tl,function(x) sum(x$pb)/length(x$pb)))
+plot(pb.sen~pb.liv)
+abline(lm(pb.sen~pb.liv))
+hist((pb.sen-pb.liv))
+t.test((pb.sen-pb.liv))
+summary(aov(pb.liv~geno.ses))
+summary(aov(pb.sen~geno.ses))
+mu <- c(liv=mean(pb.liv),sen=mean(pb.sen))
+se <- c(liv=sd(pb.liv)/sqrt(length(pb.liv)),sen=sd(pb.sen)/sqrt(length(pb.sen)))
+barplot2(mu,plot.ci=TRUE,ci.u=mu+se,ci.l=mu-se)
+pb.dif <- (pb.sen-pb.liv)
+summary(aov(pb.dif~geno.ses))
+mu <- tapply(pb.dif,geno.ses,mean)
+se <- tapply(pb.dif,geno.ses,function(x) sd(x)/sqrt(length(x)))
+barplot2(mu,plot.ci=TRUE,ci.u=mu+se,ci.l=mu-se)
+                                        #
+pbc.liv <- c(table(liv$pb[liv$pb!=0]),0)
+names(pbc.liv)[4] <- "4"
+pbc.sen <- table(sen$pb[sen$pb!=0])
+pbc <- rbind(pbc.liv,pbc.sen)
+fisher.test(pbc)
+                                        #test of the number of twos
+n2s <- tapply(pit$pb,paste(pit$tree,pit$leaf.type),function(x) length(x[x==2]))
+n2s.leaf <- unlist(sapply(names(n2s),function(x) strsplit(x,split=' ')[[1]][2]))
+summary(aov(n2s~n2s.leaf))
+                                        #fungal
+fung.liv <- unlist(lapply(liv.tl,function(x) sum(x$fungal)/length(x$fungal)))
+fung.sen <- unlist(lapply(sen.tl,function(x) sum(x$fungal)/length(x$fungal)))
+plot(fung.sen~fung.liv,pch=19)
+abline(lm(fung.sen~fung.liv))
+hist((fung.sen-fung.liv))
+t.test((fung.sen-fung.liv))
+summary(aov(fung.liv~geno.ses))
+summary(aov(fung.sen~geno.ses))
+summary(aov(I(fung.liv-fung.sen)~geno.ses))
