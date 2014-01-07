aa342a4dfb82702004be5161e3b6a0150de78933
mkl <mkl48@nau.edu>
2014-01-07 13:54:43 -0500

    update

--- projects/art_coo/src/gina_art_analyses.R
+++ projects/art_coo/src/gina_art_analyses.R
@@ -7,151 +7,77 @@ library(ecodist)
 library(rgl)
 library(mvabund)
 library(sna)
-source('~/cor_nets/CorNets.R')
+#source(ComGenR.R
 
 ###Gina's data
                                         #using 2003 data only
 gina <- read.xlsx('~/data/2000-2003 Garden Data from Gina.xls',4)
                                         #remove backcrosses
-gina <- gina[substr(gina[,1],1,2) != 'bc',]
+xtype <- substr(gina[,1],1,2)
+gina <- gina[,-1:-2]
                                         #make NAs zeros
 gina[is.na(gina)] <- 0
-                                        #mvabund
-y <- mvabund(gina[,-1:-2])
-y <- y[,apply(y,2,sum)!=0]
-x <- factor(substr(gina[,1],1,2))
-gina.mglm <- manyglm(y~x)
-plot(gina.mglm)
-gina.maov <- anova(gina.mglm)
-p.adjust(gina.maov$uni.p[2,],method='fdr')
+                                        #
+###Composition
+com <- gina[,apply(gina,2,sum)!=0]
+adonis(com~xtype)
 
-#networks
-gina.anal <- co.nets(cbind(substr(gina[,1],1,2),gina[,-1:-2]),min.dens=3)
-net <- gina.anal$cp.net
-v.cex <- (degree(net)/max(degree(net))+1)^2
-gplot(net,gmode='graph',displaylabels=TRUE,label.cex=0.5,vertex.sides=50,vertex.cex=v.cex,edge.col='grey',edge.lwd=0.65,vertex.col='darkorange',vertex.border='lightblue')
-gina.anal$spp.names[v.cex==max(v.cex)]
-colnames(net)[v.cex==max(v.cex)]
-gina.anal$spp.names[c(26,55,71,30,48,77,9)+3]
-#modules
-detach(package:sna)
-library(igraph)
-main.net. <- main.net <- net[apply(net,1,sum)!=0,apply(net,2,sum)!=0]
-main.net.[main.net.!=0] <- 1
-net.igraph <- graph.adjacency(main.net.)
-gina.clust <- clusters(net.igraph,'strong')$membership+1
-detach(package:igraph)
-library(sna)
-v.col <- heat.colors(max(gina.clust))[gina.clust]
-v.col <- grey(gina.clust/max(gina.clust))
-gplot(main.net,gmode='graph',displaylabels=TRUE,label.cex=0.5,vertex.sides=50,vertex.cex=v.cex,edge.col='grey',edge.lwd=0.65,vertex.col=v.col,vertex.border='lightblue')
+###Unipartite network
+net <- CoNetwork(com[,apply(com,2,sum)!=0])
+n.com <- nullCom(com[,apply(com,2,sum)!=0])
+n.c <- unlist(lapply(n.com,C.score))
+o.c <- C.score(com[,apply(com,2,sum)!=0])
+p.val <- length(n.c[n.c<=o.c])/length(n.c)
+ses <- (o.c-mean(n.c))/sd(n.c)
+                                        #
+v.cex <- apply(com[,apply(com,2,sum)!=0],2,sum) #scaling node size by the log of species frequencies
+v.cex <- (((v.cex/sum(v.cex))/max((v.cex/sum(v.cex))))*3)+0.1
+e.col <- net
+e.col[net>0.5] <- 'red'
+e.col[net<0.5] <- 'black'
+e.col[net==0.5] <- 'grey'
+gplot(abs(net),displaylabels=FALSE,gmode='graph',pad=1.5,
+      edge.lwd=(abs(net)),vertex.cex=v.cex,vertex.col='grey',
+      edge.col=e.col)
+
+###Bipartite Network
+xtype. <- xtype[order(apply(com,1,function(x) sum(sign(x))),decreasing=TRUE)]
+bpn <- com[order(apply(com,1,function(x) sum(sign(x))),decreasing=TRUE),order(apply(com,2,function(x) sum(sign(x))),decreasing=TRUE)]
+rownames(bpn) <- paste(xtype.,(1:nrow(bpn)),sep='_')
+plotweb(bpn,method='normal',text.rot=90,col.low=as.numeric(factor(xtype.)))
+nest.test <- oecosimu(bpn, nestfun="nestedtemp", "r1",nsimul = 999,burnin=50)
+nest.test <- oecosimu(bpn, nestfun="nestedtemp", "r00",nsimul = 999,burnin=50)
+
+###Modularity
+bpn.mods <- computeModules(bpn[,apply(bpn,2,function(x) sum(sign(x)))>1])
+plotModuleWeb(bpn.mods)
+
+###Plot
+par(mfcol=c(2,1))
+gplot(abs(net),displaylabels=FALSE,gmode='graph',pad=1.5,
+      edge.lwd=(abs(net)),vertex.cex=v.cex,vertex.col='grey',
+      edge.col=e.col)
+plotweb(bpn,method='normal',text.rot=90,col.low=as.numeric(factor(xtype.)))
 
 ###Art's Data
-  art <- list()
-art[[1]] <- read.csv('~/data/onc_arth_04.csv')
-art[[2]] <- read.csv('~/data/onc_arth_05.csv')
-art[[3]] <- read.csv('~/data/onc_arth_06.csv')
-x <- art[[2]]
+art <- list()
+art[[1]] <- read.csv('~/data/onc_arth_04.csv')[,-1]
+art[[2]] <- read.csv('~/data/onc_arth_05.csv')[,-1]
+art[[3]] <- read.csv('~/data/onc_arth_06.csv')[,-1]
+geno <- read.csv('~/data/onc_arth_04.csv')[,1]
+year <- c(rep('2004',nrow(art[[1]])),rep('2005',nrow(art[[2]])),rep('2006',nrow(art[[3]])))
+sp.rm <- apply(do.call(rbind,lapply(art,function(x) apply(x,2,function(x) sum(sign(x))))),2,function(x) all(x!=0))
+art <- do.call(rbind,art)
+art <- art[,sp.rm]
+art <- split(art,year)
+                                        #remove species that are not present in all three years
                                         #analysis
-art.anal <- lapply(art,function(x) co.nets(x,min.dens=5))
-names(art.anal) <- 2004:2006
-                                        #plots
+library(igraph)
 par(mfrow=c(1,3))
-for (i in 1:length(art)){
-  net <- art.anal[[i]]$cp.net
-  v.cex <- (degree(net)/max(degree(net))+1)^2
-  e.col <- net
-  e.col[e.col>0.5] <- 'black'
-  e.col[e.col==0.5] <- 'grey'
-  e.col[e.col<0.5] <- 'red'
-  gplot(net,gmode='graph',vertex.sides=50,displaylabels=TRUE,label.cex=0.75,vertex.col='grey',vertex.cex=v.cex,edge.col=e.col)
-  title(main=names(art.anal)[i])
-}
-                                        #correlation between degree and frequency
-my.degree <- function(x){
-  diag(x) <- 0
-  x[x!=0] <- 1
-  x <- apply(x,1,sum)
-  return(x)
-}
-par(mfrow=c(1,3))
-plot(my.degree(art.anal[[1]]$dist.net)~apply(art.anal[[1]]$x,2,sum))
-plot(my.degree(art.anal[[2]]$dist.net)~apply(art.anal[[2]]$x,2,sum))
-plot(my.degree(art.anal[[3]]$dist.net)~apply(art.anal[[3]]$x,2,sum))
-                                        #
-summary(glm(my.degree(art.anal[[1]]$dist.net)~apply(art.anal[[1]]$x,2,sum),family='poisson'))
-summary(glm(my.degree(art.anal[[2]]$dist.net)~apply(art.anal[[2]]$x,2,sum),family='poisson'))
-summary(glm(my.degree(art.anal[[3]]$dist.net)~apply(art.anal[[3]]$x,2,sum),family='poisson'))
-                                        #
-par(mfrow=c(2,2))
-plot(glm(my.degree(art.anal[[1]]$dist.net)~apply(art.anal[[1]]$x,2,sum),family='poisson'))
-plot(glm(my.degree(art.anal[[2]]$dist.net)~apply(art.anal[[2]]$x,2,sum),family='poisson'))
-plot(glm(my.degree(art.anal[[3]]$dist.net)~apply(art.anal[[3]]$x,2,sum),family='poisson'))
-
-                                        #
-n <- c(47:60)
-art.anal[[2]]$spp.names[(n+3)]
-data.frame(n,art.anal[[2]]$spp.names[(n+3)])
-n <- 7
-art.anal[[2]]$spp.names[(n+3)]
-(1:length(art.anal[[2]]$spp.names))[art.anal[[2]]$spp.names=='PB.Gall']+3
-art.anal[[2]]$spp.names[(46+3)]
-                                        #mvabund
-art.maov <- list()
-min.spp <- 5
-                                        #2004
-x <- art[[1]][,1]
-y <- mvabund(art[[1]][,-1])
-y <- y[,apply(y,2,sum) > min.spp]
-mva <- manyglm(y~x)
-par(mfrow=c(1,3))
-plot.manyglm(mva)
-meanvar.plot(y~x)
-maov <- anova(mva,p.uni='unadjusted')
-names(maov)
-art.maov[[1]] <- maov
-                                        #2005
-x <- art[[2]][,1]
-y <- mvabund(art[[2]][,-1])
-y <- y[,apply(y,2,sum) > min.spp]
-mva <- manyglm(y~x)
-par(mfrow=c(1,3))
-plot.manyglm(mva)
-meanvar.plot(y~x)
-maov <- anova(mva,p.uni='unadjusted')
-names(maov)
-maov$uni.p
-art.maov[[2]] <- maov
-                                        #2006
-x <- art[[3]][,1]
-y <- mvabund(art[[3]][,-1])
-y <- y[,apply(y,2,sum) > min.spp]
-mva <- manyglm(y~x)
-par(mfrow=c(1,3))
-plot.manyglm(mva)
-meanvar.plot(y~x)
-maov <- anova(mva,p.uni='unadjusted')
-art.maov[[3]] <- maov
-                                        #compare years
-                                        #FDR
-p.adjust(art.maov[[1]]$uni.p[2,],method='fdr')[p.adjust(art.maov[[1]]$uni.p[2,],method='fdr')<=0.10]
-p.adjust(art.maov[[2]]$uni.p[2,],method='fdr')[p.adjust(art.maov[[2]]$uni.p[2,],method='fdr')<=0.10]
-p.adjust(art.maov[[3]]$uni.p[2,],method='fdr')[p.adjust(art.maov[[3]]$uni.p[2,],method='fdr')<=0.10]
-                                        #unadjusted
-art.maov[[1]]$uni.p[2,][art.maov[[1]]$uni.p[2,]<=0.05]
-art.maov[[2]]$uni.p[2,][art.maov[[2]]$uni.p[2,]<=0.05]
-art.maov[[3]]$uni.p[2,][art.maov[[3]]$uni.p[2,]<=0.05]
-                                        #
-n <- 19
-art.anal[[2]]$spp.names[art.anal[[2]]$spp.names==art.anal[[2]]$spp.names[n+9]]
-n <- 3
-(1:length(art.anal[[n]]$spp.names))[art.anal[[n]]$spp.names=='PB.Gall']-9
-library(gplots)
-mu <- tapply(art[[1]][,colnames(art[[1]])=='PB.Gall'],art[[1]][,1],mean)
-se <- tapply(art[[1]][,colnames(art[[1]])=='PB.Gall'],art[[1]][,1],function(x) sd(x)/length(x))
-mu <- rbind(mu,tapply(art[[2]][,colnames(art[[2]])=='PB.Gall'],art[[2]][,1],mean))
-se <- rbind(se,tapply(art[[2]][,colnames(art[[2]])=='PB.Gall'],art[[2]][,1],function(x) sd(x)/length(x)))
-mu <- rbind(mu,tapply(art[[3]][,colnames(art[[3]])=='PB.Gall'],art[[3]][,1],mean))
-se <- rbind(se,tapply(art[[3]][,colnames(art[[3]])=='PB.Gall'],art[[3]][,1],function(x) sd(x)/length(x)))
-barplot2(mu,beside=TRUE,las=2,plot.ci=TRUE,ci.u=mu+se,ci.l=mu-se)
-
+art.net <- lapply(art,CoNetwork)
+                                        #test genotype effects
+art.rel <- lapply(art,function(x) apply(x,2,function(x) x/max(x)))
+art.perm <- lapply(art,function(x,g) adonis(x~g)$aov.tab,g=geno)
+artrel.perm <- lapply(art.rel,function(x,g) adonis(x~g)$aov.tab,g=geno)
+do.call(rbind,art.perm)
+do.call(rbind,artrel.perm)
