9edd0ff8323c884d65a58a5b509e1bb09533b4a2
mkl <mkl48@nau.edu>
2014-01-28 11:28:02 -0500

    ses script added

--- projects/een/src/een_get_ses.R
+++ projects/een/src/een_get_ses.R
@@ -1,5 +1,14 @@
 ###Getting SES values for een_sym
 
+library(vegan)
+library(bipartite)
+###Sourcing ComGenR on hoth
+oldwd <- getwd()
+setwd('../../../../ComGenR/R/')
+cgn.list <- (sapply(dir(),grepl,pattern='~')|sapply(dir(),grepl,pattern='\\#'))==FALSE
+sapply(dir()[cgn.list],source)
+setwd(oldwd)
+###
 x <- dget('../results/een_exp_sym/comsim_sym.rdata')
 out <- lapply(x,function(x) lapply(x,function(x) cnm.test(x,nits=5000)[1]))
 names(out) <- paste('sim',(1:length(x)),sep='')
