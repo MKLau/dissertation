8362f9a929ab16cf43f3e4f21d7b89d7feed1d0d
mkl <mkl48@nau.edu>
2014-03-21 16:48:35 -0400

    backup

---
+++ projects/lcn/src/lcn_load_onc.R
@@ -0,0 +1,76 @@
+###LCN: loading the onc data
+###MKLau
+###21Mar2014
+
+source('~/projects/packages/cooc/src/cooc.R')
+garden.data <- read.csv('~/projects/dissertation/projects/lcn/data/LCO_data_ONC_PIT.csv')
+                                        #remove genotype RL6 and N1.31
+garden.data <- garden.data[garden.data$Geno!='RL6',]
+garden.data <- garden.data[garden.data$Tree!='N1.31',]
+                                        #separate onc
+garden.data[,1] <- as.character(garden.data[,1])
+g1 <- substr(garden.data[,1],2,2)
+g1[g1!='P'] <- 'onc'
+onc <- garden.data[g1=='onc',]
+					#tree overlap between years
+unique(onc$Tree[onc$Year=='2010']) %in% unique(onc$Tree[onc$Year=='2011'])
+unique(onc$Tree[onc$Year=='2011']) %in% unique(onc$Tree[onc$Year=='2010'])
+                                        #
+if (all(table(onc[,1])==100)){print('Good to go!')}else{for (i in 1:1000){print('Warning: check input data!!!')}}
+                                        #separate trees
+onc.q <- split(onc,paste(onc[,1],onc[,2]))
+onc.q <- lapply(onc.q,function(x) x[,7:ncol(x)])
+                                        #get genotype
+onc.geno <- unlist(sapply(names(onc.q),function(x) strsplit(x,split=' ')[[1]][2]))
+                                        #Roughness in the Garden
+rough <- read.csv('../../lcn/data/ONC_raw_roughness.csv')
+rough <- rough[as.character(rough[,1])!="",1:5]
+                                        #isolate north quadrats
+rough <- rough[sapply(rough[,3],function(x) substr(x,1,1)=='N'),]
+                                        #average roughness
+avg.rough <- tapply(rough[,5],rough[,1],mean)
+r.tree <- names(avg.rough)
+r.tree <- sub('-','\\.',r.tree)
+r.tree <- sub('\\.0','\\.',r.tree)
+names(avg.rough) <- r.tree
+                                        #match roughness to to ses values
+                                        #onc.ses <- lapply(onc.q,function(x) oecosimu(x,cs,method='r1',burnin=100,thin=10,nsimul=5000))
+onc.ses <- lapply(onc.ses,function(x) x[[1]])
+ses.tree <- onc.ses
+names(ses.tree) <- names(onc.q)
+ses.tree <- as.character(sapply(names(onc.ses),function(x) unlist(strsplit(x,split=' '))[1]))
+onc.rough <- avg.rough[match(ses.tree,r.tree)]
+all(ses.tree==names(onc.rough))
+                                        #Microsat data from Nash
+## gen.d <- read.csv(file='../../lcn/data/ONC_MSAT_datafromnash.csv')[,-1]
+## gen.d[is.na(gen.d)] <- 0
+## gen.d <- as.dist((gen.d))
+                                        #RFLP distance values from Zink from Martinsen
+rflp.d <- readLines('/Users/Aeolus/projects/dissertation/projects/acn/data/rflp_queller_goodnight.txt')
+rflp.d <- lapply(rflp.d,strsplit,split='\t')
+rflp.d <- lapply(rflp.d,function(x) x[[1]])
+rflp.d[[61]] <- c(rflp.d[[61]],"")
+rflp.d <- do.call(rbind,rflp.d)
+rflp.n <- rflp.d[1,-1]
+rflp.d <- rflp.d[-1,-1]
+diag(rflp.d) <- 0
+rflp.d <- matrix(as.numeric(rflp.d),nrow=nrow(rflp.d))
+rownames(rflp.d) <- colnames(rflp.d) <- rflp.n
+rflp.d <- as.dist(rflp.d)
+                                        #community data
+onc.com <- do.call(rbind,lapply(onc.q,function(x) apply(x,2,sum)))
+onc.R <- apply(sign(onc.com),1,sum)
+onc.H <- diversity(onc.com)
+onc.com.rel <- apply(onc.com,2,function(x) x/max(x))
+onc.com.rel <- cbind(onc.com.rel,ds=rep(min(onc.com.rel[onc.com.rel!=0]),nrow(onc.com.rel)))
+onc.com <- cbind(onc.com,ds=rep(min(onc.com[onc.com!=0]),nrow(onc.com)))
+                                        #nmds and procrustes rotation
+## onc.nms <- nmds.min(nmds(vegdist(onc.com.rel),2,2))
+## onc.rot <- procrustes(dist(onc.rough),onc.nms,scale=TRUE)
+## onc.rot <- t(onc.rot$rotation)
+## onc.rot.sem <- onc.rot
+## onc.rot <- onc.rot[,(1:2)[abs(cor(cbind(onc.rough,onc.rot))[1,2:3])==max(abs(cor(cbind(onc.rough,onc.rot))[1,2:3]))]]
+
+### Renaming
+oq <- onc.q
+oc <- onc.com[,colnames(onc.com)!='ds']
---
+++ projects/lcn/src/lcn_onc.R
@@ -0,0 +1,91 @@
+###LCN: ONC Garden Analyses
+###MKLau
+###21Mar2014
+
+source('~/projects/dissertation/projects/lcn/src/lcn_load_onc.R')
+
+source('~/projects/packages/cooc/src/cooc.R')
+
+###Data objects:
+###oc = co-occurrences summed across all cells for each tree
+###oq = co-occurrence matrices separated out for each tree
+###os = matrix of ses and related stats for trees
+###wco = co-occurrence counts
+###wch = checker counts
+###prb = percent rough bark (averaged between the upper and lower)
+###Data notes:
+###No physciods
+###Lecanoras merged
+
+## Height composition
+### Previous tests show that this is not significant
+
+## Overall Co-occurrence Patterns
+#wco.all <- oecosimu(do.call(rbind,wq),cs,method='r1',burnin=100,thin=10,nsimul=5000)
+oecosimu(wc,cs,method='r1',burnin=100,thin=10,nsimul=5000)
+
+## oecosimu(wc,cs,method='r1',burnin=100,thin=10,nsimul=5000)
+##           statistic       z   mean   2.5%    50%  97.5%  Pr(sim.)    
+## statistic    1.3556 -2.9207 4.5655 2.3778 4.5556 6.7339 0.0005999
+
+## Nestedness, Modularity, Centrality
+### oecosimu(wc,nested,method='r1',burnin=100,thin=10,nsimul=5000)
+##             statistic       z   mean   2.5%    50%  97.5% Pr(sim.)   
+## binmatnest2    8.6663 -2.5715 18.592 11.589 18.428 26.445   0.0018 **
+## Only did 10
+## oecosimu(wc,mm,method='r1',burnin=100,thin=10,nsimul=10)
+##           statistic       z    mean    2.5%     50%  97.5% Pr(sim.)
+## statistic   0.18652 -1.6232 0.20923 0.18708 0.21407 0.2263   0.2727
+## mods <- computeModules(wc)
+
+plotweb(wc[order(apply(wc,1,sum),decreasing=TRUE),order(apply(wc,2,sum),decreasing=TRUE)],text.rot=90,method='normal')
+
+## SES ~ Roughness
+## prb         -0.03937    0.01809  -2.177   0.0502 .
+summary(lm(ws$wses~prb))
+
+shapiro.test(residuals(lm(ws$wses~prb)))
+plot(ws$wses~prb,xlab='Percent Rough Bark',ylab='SES',pch=19)
+abline((lm(ws$wses~prb)))
+
+## co ~ Roughness
+## Specific co-occurrence pairs are not changing with bark roughness
+## adonis(cbind(apply(wco,2,function(x) if (all(x==0)){x}else{x/max(x)}),ds=rep(0.001,nrow(wco)))~prb,permutations=5000)
+##           Df SumsOfSqs MeanSqs F.Model      R2 Pr(>F)
+## prb        1    0.2249 0.22493 0.75984 0.05955 0.5975
+## Residuals 12    3.5522 0.29602         0.94045       
+## Total     13    3.7771                 1.00000       
+
+## ch ~ Roughness
+## Pairs of checkers are changing with roughness
+## adonis(apply(wch,2,function(x) if(all(x==0)){x}else{x/max(x)})~prb,permutations=5000)
+##           Df SumsOfSqs MeanSqs F.Model      R2 Pr(>F)
+## prb        1   0.35875 0.35875  1.9225 0.13809 0.1058
+## Residuals 12   2.23925 0.18660         0.86191
+## Total     13   2.59801                 1.00000
+
+## ch ~ co ~ SES
+nco <- apply(wco,1,sum)
+nch <- apply(wch,1,sum)
+adonis(cbind(wco,ds=rep(1,nrow(wco)))~ws$wses,permutations=5000)
+adonis(wch~ws$wses,permutations=5000)
+mantel(vegdist(cbind(wco,ds=rep(0.0001,nrow(wco))))~vegdist(wch))
+summary(lm(apply(wco,2,sum)~apply(wch,2,sum)))
+                                        #co~ch
+plot(vegdist(cbind(wco,ds=rep(0.0001,nrow(wco))))~vegdist(wch),ylab='Co-Occurrence Dissimilarity',
+     xlab='Checker Dissimilarity',pch=19)
+abline(lm((vegdist(cbind(wco,ds=rep(0.0001,nrow(wco))))~vegdist(wch))))
+plot(apply(wco,2,sum)~apply(wch,2,sum))
+abline(lm(apply(wco,2,sum)~apply(wch,2,sum)))
+                                        #co~ses
+plot(dist(as.matrix(ws$wses))~vegdist(cbind(wco,ds=rep(0.0001,nrow(wco)))),xlab='Co-Occurrence Dissimilarity',
+     ylab='SES Dissimilarity',pch=19)
+abline(lm(dist(as.matrix(ws$wses))~vegdist(cbind(wco,ds=rep(0.0001,nrow(wco))))))
+                                        #ch~ses
+plot(dist(as.matrix(ws$wses))~vegdist(wch),xlab='Checker Dissimilarity',
+     ylab='SES Dissimilarity',pch=19)
+abline(lm(dist(as.matrix(ws$wses))~vegdist(wch)))
+## Araujo network
+wan <- newAraujo(do.call(rbind,wq)[,c(-4,-8)])
+wan.col <- newAraujo(do.call(rbind,wq)[,c(-4,-8)],showsign=TRUE)
+gplot(,displaylabels=TRUE,gmode='graph')
